913c09e449ad9b7c05e5a20efcfdac1ad929c70c
Malkon F <sudo777@protonmail.com>
2019-04-10 19:08:47 -0300

    Demonstrando uso herança com overrding/super

---
+++ heranca2/Machine.java
@@ -0,0 +1,13 @@
+package heranca2;
+
+public class Machine {
+
+    public void start() {
+        System.out.println("Starting...");
+    }
+
+    public void stop() {
+        System.out.println("Stopping...");
+    }
+
+}
