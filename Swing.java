79d5867dfdffbe792196dc53cc077883b830e814
Malkon F <sudo777@protonmail.com>
2019-04-11 14:40:48 -0300

    Demonstrando uso de jLabel e jFrame

---
+++ Swing.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Swing {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame(); //usando JFrame sem estender
+        window.setVisible(true);
+        window.setSize(800, 600);
+        window.setTitle("My Window");
+
+        JLabel label = new JLabel();
+        label.setText("My label");
+
+        window.add(label);
+        
+    }
+
+}
