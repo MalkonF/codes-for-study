e910393d51a31b81109eb758299c68441890ab43
Malkon F <sudo777@protonmail.com>
2019-04-12 16:42:57 -0300

    Renomeando a classe para o padrão correto

--- GetIpAddress.java
+++ GetIpAddress.java
@@ -3,7 +3,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class getIpAddress {
+public class GetIpAddress {
 
     public static void main(String[] args) throws UnknownHostException {
 
